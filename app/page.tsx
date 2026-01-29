27506f6e916f581a23b43f83d2317b6f992283a2
MarkR <99254035+ReformaMark@users.noreply.github.com>
2026-01-29 10:59:15 +0800

    added fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div className="flex "></div>;
+  return <div className="flex justify-center items-center min-h-dvh"></div>;
 }
---
+++ lib/font.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local";
+
+export const parnasoSmallMedium = localFont({
+  src: "../public/fonts/ParnasoSmall-Regular.otf",
+});
+export const parnasoSmallRegular = localFont({
+  src: "../public/fonts/ParnasoSmall-Medium.otf",
+});
+export const gtAmericaArabicRegular = localFont({
+  src: "../public/fonts/GTAmericaArabic-Regular.ttf",
+});
