91641d7becb9553410c1d3974cf38e61dd2a3a65
DrrylPns <darrylpaniss@gmail.com>
2026-01-30 05:31:41 +0800

    feat: company footer & responsive service page

---
+++ content/company.ts
@@ -0,0 +1,10 @@
+export const companyInfo = {
+    address: {
+        line1: "Salwa Road (Interchange No.17),",
+        line2: "Main Street 33 (New Industrial Area)",
+        line3: "Street No.6, Building No.73,",
+        line4: "Ar-Rayyan, P.O.Box 24023",
+    },
+    phone: "(974) 4409-9999",
+    email: "info@domoqatar.com",
+};
