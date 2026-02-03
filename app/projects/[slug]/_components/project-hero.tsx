b04b57c75b5dd859b58b98dad7e63eacf9300343
DrrylPns <darrylpaniss@gmail.com>
2026-02-03 08:40:11 +0800

    fix: build errors
    put Project props StaticImageData | string;

--- app/projects/[slug]/_components/project-hero.tsx
+++ app/projects/[slug]/_components/project-hero.tsx
@@ -4,7 +4,7 @@ import Image, { StaticImageData } from "next/image";
 import { motion } from "framer-motion";
 
 interface ProjectHeroProps {
-  image: StaticImageData;
+  image: StaticImageData | string;
   name: string;
 }
 
