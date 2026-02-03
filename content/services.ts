4c650ed5b2169e2e8f9b581262d8519f0c63005e
MarkR <99254035+ReformaMark@users.noreply.github.com>
2026-02-03 09:01:09 +0800

    refactor: improve formatting and structure of services data in services.ts

--- content/services.ts
+++ content/services.ts
@@ -1,36 +1,42 @@
-import { ContractingCoverImage, FabricShadeEngineerCoverImage, LandscapingCoverImage, ManufacturingCoverImage, TradingCoverImage } from "@/components/constants/services-image";
+import {
+  ContractingCoverImage,
+  FabricShadeEngineerCoverImage,
+  LandscapingCoverImage,
+  ManufacturingCoverImage,
+  TradingCoverImage,
+} from "@/components/constants/services-image";
 
 export const servicesData = [
-    {
-        title: "Contracting",
-        paragraph1:
-            "Domopan is capable of undertaking contracting works of any scope or sector. This is made possible due to a unique combination of extensive experience in the region, capable workforce of over 6000 professionals, in-house production capabilities, strong partnerships with industry leaders and a diversified service portfolio.",
-        paragraph2:
-            "Our fit-outs and finishing divisions further enhance our capabilities, with over 350 personnel dedicated exclusively to these divisions.",
-        image: ContractingCoverImage,
-    },
-    {
-        title: "Manufacturing",
-        paragraph1: "lorem1",
-        paragraph2: "lorem2",
-        image: ManufacturingCoverImage,
-    },
-    {
-        title: "Landscaping",
-        paragraph1: "lorem1",
-        paragraph2: "lorem2",
-        image: LandscapingCoverImage,
-    },
-    {
-        title: "Trading",
-        paragraph1: "lorem1",
-        paragraph2: "lorem2",
-        image: TradingCoverImage,
-    },
-    {
-        title: "Fabric Shade Engineering",
-        paragraph1: "lorem1",
-        paragraph2: "lorem2",
-        image: FabricShadeEngineerCoverImage,
-    },
+  {
+    title: "Contracting",
+    paragraph1:
+      "Domopan is capable of undertaking contracting works of any scope or sector. This is made possible due to a unique combination of extensive experience in the region, capable workforce of over 6000 professionals, in-house production capabilities, strong partnerships with industry leaders and a diversified service portfolio.",
+    paragraph2:
+      "Our fit-outs and finishing divisions further enhance our capabilities, with over 350 personnel dedicated exclusively to these divisions.",
+    image: ContractingCoverImage,
+  },
+  {
+    title: "Manufacturing",
+    paragraph1: "Details unavailable.",
+    paragraph2: "Details unavailable.",
+    image: ManufacturingCoverImage,
+  },
+  {
+    title: "Landscaping",
+    paragraph1: "Details unavailable.",
+    paragraph2: "Details unavailable.",
+    image: LandscapingCoverImage,
+  },
+  {
+    title: "Trading",
+    paragraph1: "Details unavailable.",
+    paragraph2: "Details unavailable.",
+    image: TradingCoverImage,
+  },
+  {
+    title: "Fabric Shade Engineering",
+    paragraph1: "Details unavailable.",
+    paragraph2: "Details unavailable.",
+    image: FabricShadeEngineerCoverImage,
+  },
 ];
